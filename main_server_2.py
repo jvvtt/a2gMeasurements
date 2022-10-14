c21092f510b4107858e93199123116a0302156f2
JulianVillegasAtVtt <julian.villegas@vtt.fi>
2022-10-14 10:49:10 +0300

    Last version

--- main_server_2.py
+++ main_server_2.py
@@ -4,7 +4,7 @@ from socket import socket
 import time
 from a2gmeasurements import HelperA2GMeasurements, GimbalRS2, GpsSignaling
 
-input('Start Experiment?')
+meas_number = input('To start experiment, enter the number of the Measurement according to Flight Plan')
 
 host = 'localhost'
 myHelper = HelperA2GMeasurements('GROUND', host, DBG_LVL_0=False, DBG_LVL_1=True, IsGPS=True, IsGimbal=True)
@@ -12,7 +12,58 @@ myHelper.HelperStartA2GCom()
 print('Starting SERVER...')
 time.sleep(1)
 
-#
+# ---------------------------Pointing GROUND gimbal to DRONE------------------------------------------------
+# 1. Send GET_GPS
+myHelper.a2g_conn.sendall(json.dumps('GET_GPS').encode())
+
+# 2. Request Gimbal orientation
+myHelper.myGimbal.request_current_position()
+
+# Gimbal assumed to be not moving
+yaw_now = myHelper.myGimbal.yaw
+roll_now = myHelper.myGimbal.roll
+print('Compare yaw printed value previously, with yaw_now var: ', yaw_now, '\n')
+print('Compare roll printed value previously, with roll_now var: ', roll_now, '\n')
+
+# 3. Wait and block for DRONE position answer
+while(myHelper.SOCKET_BUFFER==[]):
+    1
+# 4. Get DRONE coordinates    
+lat_drone = float(myHelper.SOCKET_BUFFER[-1]['Latitude'])/10
+lon_drone = float(myHelper.SOCKET_BUFFER[-1]['Longitude'])/10
+height_drone = float(myHelper.SOCKET_BUFFER[-1]['Antenna Alt above sea level (mean)'])
+
+# 5. Get GROUND (own) coordinates
+while(myHelper.mySeptentrioGPS.NMEA_buffer == []):
+    1
+    
+lat_ground = float(myHelper.mySeptentrioGPS.NMEA_buffer[-1]['Latitude'])/10
+lon_ground = float(myHelper.mySeptentrioGPS.NMEA_buffer[-1]['Longitude'])/10
+height_ground = float(myHelper.mySeptentrioGPS.NMEA_buffer[-1]['Antenna Alt above sea level (mean)'])
+
+# 6. Point GROUND gimbal towards DRONE
+yaw, roll = myHelper.mobile_gimbal_follows_drone(yaw_now, lat_ground, lon_ground, height_ground, 
+                                                lat_drone, lon_drone, height_drone)
+
+myHelper.myGimbal.setPosControl(yaw, roll, 0)
+
+# ------------------------------- Angular movement and save experiments --------------------------------
+# 7. Wait 
+input('Press ENTER when GROUND gimbal finishes its pointing towards DRONE')
+
+# 8. Send START_SA message to DRONE
+myHelper.a2g_conn.sendall(json.dumps('START_SA').encode())
+
+# 9. Request Gimbal orientation
+myHelper.myGimbal.request_current_position()
+
+# Gimbal assumed to be not moving
+yaw_now = myHelper.myGimbal.yaw
+print('Compare yaw printed value previously, with yaw_now var: ', yaw_now, '\n')
+
+# 10. Move GROUND gimbal in 30 deg steps
+N_ang_steps = 360/30
+myHelper.send_N_azimuth_angles(int(yaw_now*10), int(N_ang_steps), meas_number=meas_number)
 
 input('Stop test?')
 myHelper.HelperA2GStopCom()
