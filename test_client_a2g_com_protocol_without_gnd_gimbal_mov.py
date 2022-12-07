38f6092df119a00077d8332830cd2b2e282081f2
JulianVillegasAtVtt <julian.villegas@vtt.fi>
2022-12-07 17:01:47 +0200

    server and client scripts for test a2g protocol

--- test_client_a2g_com_protocol_without_gnd_gimbal_mov.py
+++ test_client_a2g_com_protocol_without_gnd_gimbal_mov.py
@@ -1,6 +1,16 @@
 from a2gmeasurements import HelperA2GMeasurements
 from a2gUtils import geodetic2geocentric
+import time
+import numpy as np
+import threading
+cnt = 1
+emulated_drone_coords = [[60.18592, 24.81174, 50], #'HiQ'
+                         [60.18650, 24.81350, 50], # 'FutHub'
+                         [60.18555, 24.82041, 60], # 'FatLiz'
+                         [60.18495, 24.82302, 70]] # 'AaltoMetro'
 
+SERVER_ADDRESS = '192.168.0.2'
+drone_a2g_helper = HelperA2GMeasurements('DRONE', SERVER_ADDRESS, DBG_LVL_0=True, DBG_LVL_1=True, IsGPS=True)
 
 def emulate_drone_sbf_gps_coords():
     '''
@@ -12,23 +22,25 @@ def emulate_drone_sbf_gps_coords():
     Aalto Yliopisto Metro Google maps mark: 60.18495, 24.82302
 
     '''
+    if 'X' in drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1] and 'Y' in drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1] and 'Z' in drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1]:
+        mod_len_em_coords = np.mod(cnt, len(emulated_drone_coords))
 
-    emulated_drone_coords = {'HiQ': [60.18592, 24.81174, 50], 
-                             'FutHub': [60.18650, 24.81350, 50],
-                             'FatLiz': [60.18555, 24.82041, 60],
-                             'AaltoMetro': [60.18495, 24.82302, 70]}
+        X,Y,Z = geodetic2geocentric(emulated_drone_coords[int(mod_len_em_coords)][0], 
+                                    emulated_drone_coords[int(mod_len_em_coords)][1], 
+                                    emulated_drone_coords[int(mod_len_em_coords)][2])
+        drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1]['X'] =  X
+        drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1]['Y'] = Y
+        drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1]['Z'] = Z
+        drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1]['Datum'] =  0
 
-    for key, val in emulate_drone_sbf_gps_coords.items():
-        X,Y,Z = geodetic2geocentric(val[0], val[1], val[2])
-        em_drone_gps_msg_data = {'X': X, 'Y': Y, 'Z': Z, 'Datum': 0}
+for i in range(10):    
+    emulate_drone_sbf_gps_coords()    
+    
+    print('\nCNT: ', cnt)
+    print(f"\nTHIS ({drone_a2g_helper.ID}) emulated buff length: {len(drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer)}")
+    #print(drone_a2g_helper.mySeptentrioGPS.SBF_frame_buffer[-1])
+    
+    cnt = cnt + 1    
 
-SERVER_ADDRESS = '192.168.0.2'
-drone_a2g_helper = HelperA2GMeasurements('DRONE', SERVER_ADDRESS, DBG_LVL_0=True, DBG_LVL_1=True)
-
-
-drone_a2g_helper.HelperStartA2GCom()
-
-# Update the emulated drone position
-#drone_a2g_helper.
-
-
+drone_a2g_helper.mySeptentrioGPS.stop_gps_data_retrieval()
+drone_a2g_helper.mySeptentrioGPS.stop_thread_gps()
