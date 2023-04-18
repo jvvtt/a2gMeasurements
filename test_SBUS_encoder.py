6f57ae4f5204c0e12637f084394c093429b73bc6
Julian Villegas <julian.villegas@vtt.fi>
2023-04-18 12:53:52 +0200

    SBUS more changes

--- test_SBUS_encoder.py
+++ test_SBUS_encoder.py
@@ -4,63 +4,26 @@ import time
 
 list_of_channels = {'MODE': 5, 'TILT': 2, 'ROLL': 4, 'PAN': 1, 'TILT_SPEED': 9, 'PAN_SPEED': 10}
 
-
-def not_move_command(sbus):
-    '''
-    Update the channel so that it does not continue moving
-
-    '''
+def ctrl_H16_position(sbus):
     
-    sbus.update_channel(channel=1, value=0)
-    sbus.update_channel(channel=2, value=0)
-    sbus.update_channel(channel=3, value=0)
-    sbus.update_channel(channel=4, value=0)
-    sbus.update_channel(channel=5, value=0)
-    #time.sleep(0.1)
-
-def test_Y_axis(sbus, ail, mov_time):
-    '''
-    Test the roll axis speed
-
-    '''
-
-    sbus.update_channel(channel=1, value=ail)
-    sbus.update_channel(channel=2, value=0)
-    sbus.update_channel(channel=3, value=0)
-    sbus.update_channel(channel=4, value=0)
-    sbus.update_channel(channel=5, value=0)
-    time.sleep(mov_time)
-    not_move_command(sbus)
-
-
-def test_Z_axis(sbus, ele, mov_time):
-    '''
-    Test the pitch axis speed
-
-    '''
+    condition = True
     
-    sbus.update_channel(channel=1, value=0)
-    sbus.update_channel(channel=2, value=ele)
-    sbus.update_channel(channel=3, value=0)
-    sbus.update_channel(channel=4, value=0)
-    sbus.update_channel(channel=5, value=0)
-    time.sleep(mov_time)
-    not_move_command(sbus)
-
-def test_pan_axis(sbus, rud, mov_time):
-    '''
-    Test the pan axis speed
-    '''
+    while(condition):
+        ele =  input('ENTER elevation speed: ')
+        rud = input('ENTER pan speed: ')
+        mov_time = input('ENTER time: ')
+            
+        ele = float(ele)
+        rud = float(rud)
+        mov_time = float(mov_time)
+        
+        sbus.move_gimbal(ele, rud, mov_time)
+        
+        finish = input('Has it finished?: ')
+        
+        if finish == 'YES' or finish == 'Yes' or finish == 'yes' or finish == 'Y' or finish == 'y':
+            condition = False
     
-    sbus.update_channel(channel=1, value=0)
-    sbus.update_channel(channel=2, value=0)
-    sbus.update_channel(channel=3, value=0)
-    sbus.update_channel(channel=4, value=rud)
-    sbus.update_channel(channel=5, value=0)
-    time.sleep(mov_time)
-    not_move_command(sbus)
-
-
 sbus = SBUSEncoder()
 
 serial_interface = input('\nType serial interface:')
@@ -70,22 +33,35 @@ sbus.start_sbus(serial_interface=serial_interface, period_packet=0.007)
 
 time.sleep(3)
 
-###### CHANGE FROME HERE ########
+ctrl_H16_position(sbus)
 
-'''
-rud = -20
-mov_time_pan_axis = 20
-test_pan_axis(sbus, rud, mov_time_pan_axis)
-input('\nPress ENTER to continue to the next test')
-'''
+experiment_not_finish = True
 
-ele = 20
-mov_time_elevation_axis = 5
-test_Z_axis(sbus, ele, mov_time_elevation_axis)
-#input('\nPress ENTER to continue to the next test')
-
-
-#### UNTIL HERE YOU CAN CHANGE #######
+rud = 0
+while(experiment_not_finish):
+    ele = input('ENTER elevation speed: ')
+    ele = float(ele)
+    
+    condition = True
+    while(condition):       
+        print('\nACTUAL ELEVATION: ' + str(ele)) 
+        mov_time = input('ENTER time: ')
+        mov_time = float(mov_time)
+        
+        sbus.move_gimbal(ele, rud, mov_time)
+        
+        finnish = input('Time founded?: ')
+        
+        if finnish == 'YES' or finnish == 'Yes' or finnish == 'yes' or finnish  == 'Y' or finnish == 'y':
+            condition = False
+        if finnish == 'NO' or finnish == 'No' or finnish =='no' or finnish == 'N' or finnish == 'n':
+            ctrl_H16_position(sbus)
+    
+    finish_experiment = input('Finish experiment?: ')
+    
+    if finish_experiment == 'YES' or finish_experiment == 'Yes' or finish_experiment == 'yes' or finish_experiment == 'Y' or finish_experiment == 'y':
+        experiment_not_finish = False
+                    
 print('\nEnd of tests')
 sbus.stop_updating()
 
